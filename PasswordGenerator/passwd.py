5422d2e119e8097bcc570058a519a79d409c5947
prox11 <148764185+prox11@users.noreply.github.com>
2023-11-21 12:16:22 +0100

    Update passwd.py

--- PasswordGenerator/passwd.py
+++ PasswordGenerator/passwd.py
@@ -1,52 +1,70 @@
 import random
 import string
+import tkinter as ttk
 
-lista1 = list(string.ascii_lowercase)
-lista2 = list(string.ascii_uppercase)
-lista3 = list(string.digits)
-lista4 = list(string.punctuation)
+def jelszo_gen():
+    lista1 = list(string.ascii_lowercase)
+    lista2 = list(string.ascii_uppercase)
+    lista3 = list(string.digits)
+    lista4 = list(string.punctuation)
 
-print("Üdv a jelszó generáló programomban.")
+    db = bevitel.get()
 
-db = input("Milyen hosszúságú legyen a jelszó: ")
+    while True:
+        try:
+            szam = int(db)
+            if szam < 8:
+                result_label.config(text="Legalább 8 karakter hosszú legyen.")
+                return
+            elif szam >= 53:
+                result_label.config(text="Legfeljebb 52 karakter lehet.")
+                return
+            else:
+                break
+        except:
+            result_label.config(text="Nem megfelelő érték")
+            return
 
+    random.shuffle(lista1)
+    random.shuffle(lista2)
+    random.shuffle(lista3)
+    random.shuffle(lista4)
 
-while True:
-    try:
-        szam = int(db)
-        if szam < 8:
-            print("Legalább 8 karakter hosszúságú legyen.")
-            db = input("Kérem adja meg a hosszúságot újra: ")
-        elif szam >= 53:
-            print("Kisebb legyen mint 53 karakter. ")
-            db = input("Kérem adja meg a hosszúságot újra: ")
-        else:
-            break
-    except:
-        print("Nem megfelelő érték")
-        db = input("Kérem adjon meg egy egész számot: ")
+    eredmeny = []
 
-random.shuffle(lista1)
-random.shuffle(lista2)
-random.shuffle(lista3)
-random.shuffle(lista4)
+    part1 = round(szam * (30/100))
+    part2 = round(szam * (20/100))
 
-eredmeny = []
+    for i in range(part1):
+        eredmeny.append(lista1[i])
+        eredmeny.append(lista2[i])
 
-part1 = round(szam * (30/100))
-part2 = round(szam * (20/100))
+    for i in range(part2):
+        eredmeny.append(lista3[i])
+        eredmeny.append(lista4[i])
 
-for i in range(part1):
-    eredmeny.append(lista1[i])
-    eredmeny.append(lista2[i])
+    random.shuffle(eredmeny)
 
-for i in range(part2):
-    eredmeny.append(lista3[i])
-    eredmeny.append(lista4[i])
-
-random.shuffle(eredmeny)
-
-passwd = "".join(eredmeny)
-print(f"Az eredmény: {passwd}")
+    passwd = "".join(eredmeny)
+    result_label.config(text=f"A generált jelszó: {passwd}")
 
 
+root = ttk.Tk()
+root.title("Jelszó generátor ablak")
+root.geometry("600x300")
+
+root.configure(bg='#FFA500')
+
+szoveg = ttk.Label(root, text="Üdv a jelszó generáló ablakban!\r\n Milyen hosszú legyen a jelszó?", bg='#FFA500', fg='black')
+szoveg.pack()
+
+bevitel = ttk.Entry(root)
+bevitel.pack()
+
+gomb = ttk.Button(root, text="Generálás", command=jelszo_gen, bg='black', fg='white')
+gomb.pack(pady=10)
+
+result_label = ttk.Label(root, text="", bg='#FFA500', fg='black')
+result_label.pack()
+
+root.mainloop()
